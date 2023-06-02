5f45d9e0c2e2c77e41dd2b131d561dc0e495676f
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-02 15:50:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+
+function getRandomTime(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let i = 0; i < 5; i++) {
+  const promise = new Promise((resolve) => {
+    const time = getRandomTime(1000, 5000);
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${time / 1000} seconds`);
+    }, time);
+  });
+  
+  promises.push(promise);
+}
+
+Promise.any(promises)
+  .then((result) => {
+    document.getElementById("output").innerText = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
